c271dcbba79d826528886f8e04eb9df2b97ad72a
InkyChew <B10623019@yuntech.edu.tw>
2020-10-07 18:46:32 +0800

    set linebot

--- app.py
+++ app.py
@@ -32,8 +32,10 @@ ma.init_app(app)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+# line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+# handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+line_bot_api = LineBotApi("8Qx/5LGcmGbpSqYhKkSWIu6sszJi312Wp24lxzEVzWOD2Yd9AJIvXvGxRgbvxySgutpOGUBVZ4s36FIpaThB1YvVQBC82pguJ1FMAsLQVTu7X8c19r8SG+mQoBwF7UF8qaiZD2ySjEshb0QKrFL2RQdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("1252c2b560ea5a8ff78d852764e351ec")
 
 @app.route('/')
 def index():
