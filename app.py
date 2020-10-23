41daded9192615d100f13423f79768ea7a4b932f
InkyChew <B10623019@yuntech.edu.tw>
2020-10-23 17:28:46 +0800

    linebot set

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 @app.route('/')
 def index():
-  db.create_all()
+  # db.create_all()
   print(line_bot_api)
   print(handler)
   return 'ok'
